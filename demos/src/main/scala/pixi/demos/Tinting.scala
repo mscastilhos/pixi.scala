219a1213d57a530c1db5a5ad38c1d6975dc6af9b
Marcelo Castilhos <mscastilhos@gmail.com>
2015-06-03 15:50:09 +0100

    Tinting demo

--- demos/src/main/scala/pixi/demos/Tinting.scala
+++ demos/src/main/scala/pixi/demos/Tinting.scala
@@ -2,56 +2,58 @@ package pixi.demos
 
 import org.scalajs.dom
 import pixi.core.{Rectangle, Sprite, Container, Renderer}
-import pixi.scalajs.UserData.WithUserData
 
 import scala.scalajs.js.annotation.JSExport
+import pixi.scalajs.UserData.WithUserData
 
-@JSExport("BlendModes")
-object BlendModes {
+@JSExport("Tinting")
+object Tinting {
   val renderer = Renderer.autoDetect(800, 600)
   dom.document.body.appendChild(renderer.view)
 
   // create the root of the scene graph
   val stage = new Container
 
-  // create a new background sprite
-  val background = Sprite.fromImage("_assets/BGrotate.jpg")
-  stage.addChild(background)
+  // Data associated with the Aliens
+  case class AlienData(var direction: Double, var turningSpeed: Double, var speed: Double)
 
-  // Data associated with the dudes
-  case class DudeData(var direction: Double, var turningSpeed: Double, var speed: Double)
-
-  // create an array to store a reference to the dudes
-  val dudeArray = for (i <- 0 until 20) yield {
+  // holder to store the aliens
+  val aliens = for (i <- 0 until 20) yield {
     // create a new Sprite that uses the image name that we just generated as its source
-    val dude = Sprite.fromImage("_assets/flowerTop.png")
+    val dude = Sprite.fromImage("_assets/eggHead.png")
 
+    // set the anchor point so the texture is centerd on the sprite
     dude.anchor.set(0.5)
 
-    // set a random scale for the dude
+    // set a random scale for the dude - no point them all being the same size!
     dude.scale.set(0.8 + Math.random() * 0.3)
 
-    // finally let"s set the dude to be at a random position...
-    dude.position.x = Math.floor(Math.random() * renderer.width)
-    dude.position.y = Math.floor(Math.random() * renderer.height)
+    // finally lets set the dude to be at a random position..
+    dude.position.x = Math.random() * renderer.width
+    dude.position.y = Math.random() * renderer.height
 
-    // The important bit of this example, this is how you change the default blend mode of the sprite
-    dude.blendMode = pixi.Consts.BLEND_MODES.ADD
+    dude.tint = Math.random() * 0xFFFFFF
 
-    // create some extra properties that will control movement
-    val data = DudeData(direction = Math.random() * Math.PI * 2,
+    // create some extra properties that will control movement :
+    val data = AlienData(
+      // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
+      direction = Math.random() * Math.PI * 2,
+
+      // this number will be used to modify the direction of the dude over time
       turningSpeed = Math.random() - 0.8,
-      speed = 2 + Math.random() * 2)
+
+      // create a random speed for the dude between 0 - 2
+      speed = 2 + Math.random() * 2
+    )
 
     stage.addChild(dude)
 
-    // finally return the dude with extra powers into the dudeArray so it it can be easily accessed later
+    // finally we yield the dude into the aliens array so it it can be easily accessed later
     dude.withUserData(data)
   }
 
-  // create a bounding box box for the little dudes
+  // create a bounding box for the little dudes
   val dudeBoundsPadding = 100
-
   val dudeBounds = new Rectangle(-dudeBoundsPadding,
     -dudeBoundsPadding,
     renderer.width + dudeBoundsPadding * 2,
@@ -61,10 +63,10 @@ object BlendModes {
 
   dom.requestAnimationFrame(animate _)
 
+  def animate(d: Double) {
 
-  def animate(d: Double = 0) {
-    // iterate through the dudes and update the positions
-    for (dude <- dudeArray) {
+    // iterate through the dudes and update their position
+    for (dude <- aliens) {
       dude.userData.direction += dude.userData.turningSpeed * 0.01
       dude.position.x += Math.sin(dude.userData.direction) * dude.userData.speed
       dude.position.y += Math.cos(dude.userData.direction) * dude.userData.speed
@@ -87,7 +89,7 @@ object BlendModes {
     // increment the ticker
     tick += 0.1
 
-    // time to render the stage !
+    // time to render the stage!
     renderer.render(stage)
 
     // request another animation frame...
